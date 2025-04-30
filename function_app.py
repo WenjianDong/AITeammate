89e50cc34b1582d07d705f5ed33d63ee3cdf355b
Wenjian Dong <dwj@Mac.lan>
2025-04-30 18:57:17 +0200

    switch back to LLM

--- function_app.py
+++ function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 import logging
 import os
-#from openai import OpenAI
+from openai import OpenAI
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -28,18 +28,19 @@ def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
     Please generate 12 encouragement. 
     """
 
-    # client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
-    # response = client.responses.create(
-    #     model="gpt-3.5-turbo",
-    #     input=prompt
-    # )
-    # output_text = response.output_text
-    output_text = 'hhh'
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    response = client.responses.create(
+        model="gpt-3.5-turbo",
+        input=prompt
+    )
+    #output_text = response.output_text
+    output_text = response.output[0].content[0].text
+    #output_text = 'hhh'
 
     if output_text:
         return func.HttpResponse(output_text)
     else:
         return func.HttpResponse(
              "This HTTP triggered the function, but some error happened when executing function app.",
-             status_code=200
+             status_code=202
         )
